0b7b39d2e921339729fdfd58ca593c8fb9c824f0
jairo <jairo_cal@yahoo.com>
2021-05-07 16:37:40 -0700

    fixed rendering of vidoes on inital load. Now you can change what videos to render on click

--- react-app/src/components/Videos/Videos.js
+++ react-app/src/components/Videos/Videos.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 // Import Dispatches
 import { getFriendsFeed } from "../../store/friendsfeed";
-import { getAllVideos } from '../../store/allvideos'
-import { getCategoriesFeed } from '../../store/foryou'
+import { getAllVideos } from "../../store/allvideos";
+import { getCategoriesFeed } from "../../store/foryou";
 import "./videos.css";
 
 function Videos() {
@@ -12,8 +12,8 @@ function Videos() {
   const allFeed = useSelector((state) => state.allVideos);
   const friendsFeed = useSelector((state) => state.friendsFeed);
   const categoriesFeed = useSelector((state) => state.categoriesFeed);
-  const [videos, setVideos] = useState(allFeed)
-  
+  const [page, setPage] = useState(0);
+
   // grabs the Friends Feed videos
   useEffect(() => {
     if (user) {
@@ -30,23 +30,23 @@ function Videos() {
 
   // grabs all videos
   useEffect(() => {
-      dispatch(getAllVideos());
+    dispatch(getAllVideos());
   }, [dispatch]);
 
   const onForYou = async (e) => {
     e.preventDefault();
-    setVideos(friendsFeed)
-  }
+    setPage(2);
+  };
 
   const onFriendsFeed = async (e) => {
     e.preventDefault();
-    setVideos(categoriesFeed)
-  }
+    setPage(1);
+  };
 
   const onAllFeed = async (e) => {
     e.preventDefault();
-    setVideos(allFeed)
-  }
+    setPage(0);
+  };
 
   return (
     <div className="right_nav">
@@ -59,17 +59,39 @@ function Videos() {
       <div>
         <button onClick={onAllFeed}>Explore</button>
       </div>
-      {console.log(videos)}
-      <div className="video_container">
-        {videos.length > 0 &&
-          videos.map((video) => (
+      {allFeed.length > 0 && page === 0 && (
+        <div className="video_container">
+          {allFeed?.map((video) => (
             <div>
-              <video width="500px" src={video.video_url} controls></video>
+              <video width="100%" src={video.video_url} controls></video>
               <p>{video.title}</p>
               <p>{video.description}</p>
             </div>
           ))}
-      </div>
+        </div>
+      )}
+      {friendsFeed.length > 0 && page === 1 && (
+        <div className="video_container">
+          {friendsFeed?.map((video) => (
+            <div>
+              <video width="100%" src={video.video_url} controls></video>
+              <p>{video.title}</p>
+              <p>{video.description}</p>
+            </div>
+          ))}
+        </div>
+      )}
+      {categoriesFeed.length > 0 && page === 2 && (
+        <div className="video_container">
+          {categoriesFeed?.map((video) => (
+            <div>
+              <video width="100%" src={video.video_url} controls></video>
+              <p>{video.title}</p>
+              <p>{video.description}</p>
+            </div>
+          ))}
+        </div>
+      )}
       <div className="comments_container">
         <div className="comments_header">
           <h1>Comments!</h1>
