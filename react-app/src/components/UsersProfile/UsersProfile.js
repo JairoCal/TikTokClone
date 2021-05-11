e3e4a3bed7eea7fa1badf80ffab5c4dbd0f4e250
jairo <jairo_cal@yahoo.com>
2021-05-11 14:48:46 -0700

    other users profile page now shows information about them

--- react-app/src/components/UsersProfile/UsersProfile.js
+++ react-app/src/components/UsersProfile/UsersProfile.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { getUserInfo } from "../../store/UserInfo";
 
 function UsersProfile() {
+  const user = useSelector((state) => state.userInfo);
   // user id for the persons page we want to see
   const { user_id } = useParams();
   const dispatch = useDispatch();
@@ -13,9 +14,23 @@ function UsersProfile() {
   }, [dispatch, user_id]);
 
   return (
-    <div>
+    <div className="profile_container">
       <style type="text/css">{`.left_navbar {display: none}`}</style>
-      <h1>Hi</h1>
+      {user && (
+        <div>
+        {console.log(user,"----------------------------")}
+          <div className="profile_user_info">
+            <img className="profile_user_image" src={user.profile_image}></img>
+            <p>@{user.username}</p>
+          </div>
+          <div className="profile_my_videos_section">
+            <h1>My Videos</h1>
+            <div className="profile_my_videos_container">
+              <h1>My videos will go here displayed with a flex-wrap</h1>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
