7ef6eb8257d0d5519468bb019d33d576ffff8ce9
jairo <jairo_cal@yahoo.com>
2021-05-21 00:42:18 -0700

    added videos from other users to their pages

--- react-app/src/components/UsersProfile/UsersProfile.js
+++ react-app/src/components/UsersProfile/UsersProfile.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getUserInfo } from "../../store/UserInfo";
+import { getUserVideos } from "../../store/UsersVideos";
 
 function UsersProfile() {
   const user = useSelector((state) => state.userInfo);
@@ -9,9 +10,45 @@ function UsersProfile() {
   const { user_id } = useParams();
   const dispatch = useDispatch();
 
+  const [playing, setPlaying] = useState(false);
+  const userVideos = useSelector((state) => state.UserVideos);
+  const videoRef = useRef(null);
+
   useEffect(() => {
     dispatch(getUserInfo(user_id));
   }, [dispatch, user_id]);
+
+  useEffect(() => {
+    dispatch(getUserVideos(Number(user_id)));
+  }, [dispatch, user_id]);
+
+  const onVideoClick = () => {
+    videoRef.current.play();
+    setPlaying(true);
+  };
+
+  const onClick = (e) => {
+    // if a video is playing and we clicked on the same video as the first time
+    // then pause that video and set it to false
+    if (playing && videoRef.current === e.target) {
+      videoRef.current.pause();
+      setPlaying(false);
+    }
+    // if a video is playing but we clicked on a different video than the first time
+    // then we pause the video that was previously clicked and play new one and set to current
+    else if (playing && videoRef.current !== e.target) {
+      videoRef.current.pause();
+      setPlaying(false);
+      videoRef.current = e.target;
+      videoRef.current.play();
+      setPlaying(true);
+    }
+    // on no videos playing we trigger this which will then set a new current and play it
+    else {
+      videoRef.current = e.target;
+      onVideoClick();
+    }
+  };
 
   return (
     <div className="profile_container">
@@ -29,7 +66,19 @@ function UsersProfile() {
           <div className="profile_my_videos_section">
             <h1>My Videos</h1>
             <div className="profile_my_videos_container">
-              <h1>Users videos coming soon..</h1>
+              {userVideos.length > 0 &&
+                userVideos?.map((video) => (
+                  <div className="profile_video_holder">
+                    <video
+                      id={video.id}
+                      key={video.id}
+                      onClick={onClick}
+                      className="video_player_user"
+                      src={video.video_url}
+                      ref={videoRef}
+                    ></video>
+                  </div>
+                ))}
             </div>
           </div>
         </div>
